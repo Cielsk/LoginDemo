ec6f8c0aa9aad7fd89679162a6382a46cc58c268
Ciel Yang <cielsk43@gmail.com>
2017-12-02 16:51:22 +0800

    添加 SharedPreferences 操作工具类

---
+++ app/src/main/java/com/cielyang/android/login/common/util/SharedPreferencesUtil.java
@@ -0,0 +1,55 @@
+package com.cielyang.android.login.common.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.orhanobut.logger.Logger;
+
+/** */
+public class SharedPreferencesUtil {
+
+    private SharedPreferences mPreferences;
+
+    public SharedPreferencesUtil(Context context, String fileName) {
+        mPreferences = context.getSharedPreferences(fileName, Context.MODE_PRIVATE);
+    }
+
+    /**
+     * 保存字符串键值对.
+     *
+     * @param key   the key
+     * @param value the value
+     */
+    public void save(String key, String value) {
+        mPreferences.edit().putString(key, value).apply();
+    }
+
+    public void save(String key, Object o) {
+        String value = new Gson().toJson(o);
+        save(key, value);
+    }
+
+    public String get(String key) {
+        return mPreferences.getString(key, null);
+    }
+
+    /**
+     * 根据 key 获取对应的对象，对象由 Gson 解析而来.
+     *
+     * @param key the key
+     * @param cls 对象类型信息
+     * @return the object
+     */
+    public Object get(String key, Class<Object> cls) {
+        String value = get(key);
+        try {
+            if (value != null) {
+                return new Gson().fromJson(value, cls);
+            }
+        } catch (Exception e) {
+            Logger.e(e.getMessage());
+        }
+        return null;
+    }
+}
