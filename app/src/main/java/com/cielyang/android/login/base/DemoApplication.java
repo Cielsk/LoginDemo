337a3c3b8025d2e923e9198e19c24582ebabb82e
Ciel Yang <cielsk43@gmail.com>
2017-12-02 18:31:14 +0800

    修改 Logger 初始化设置

--- app/src/main/java/com/cielyang/android/login/base/DemoApplication.java
+++ app/src/main/java/com/cielyang/android/login/base/DemoApplication.java
@@ -3,18 +3,29 @@ package com.cielyang.android.login.base;
 import android.app.Application;
 
 import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.BuildConfig;
+import com.orhanobut.logger.FormatStrategy;
 import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
 
-/**
- *
- */
+/** */
 public class DemoApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        // 日志管理工具初始化
-        Logger.addLogAdapter(new AndroidLogAdapter());
+        initLogger();
+    }
+
+    private void initLogger() {
+        FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder().tag("Login demo").build();
+        Logger.addLogAdapter(
+                new AndroidLogAdapter(formatStrategy) {
+                    @Override
+                    public boolean isLoggable(int priority, String tag) {
+                        return BuildConfig.DEBUG;
+                    }
+                });
     }
 }
