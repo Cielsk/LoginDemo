1e4705859c4c735c06f5ce1da40b9cf03c583f23
Ciel Yang <cielsk43@gmail.com>
2017-12-02 17:57:38 +0800

    添加 SharedPreferences 设置

---
+++ app/src/main/java/com/cielyang/android/login/configs/SharedPreferences.java
@@ -0,0 +1,11 @@
+package com.cielyang.android.login.configs;
+
+/**
+ *
+ */
+public class SharedPreferences {
+
+    public static final String TOKEN_FILE_NMAE = "session_token";
+
+    public static final String TOKEN_KEY = "token";
+}
